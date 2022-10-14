dde57246ed5149462e180dd291ad1d9f3d23a56a
Ben Frey <spartanhooah@gmail.com>
2022-10-14 13:16:54 -0400

    finish web map

--- map1.py
+++ map1.py
@@ -2,13 +2,13 @@ import folium
 import pandas
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
-features = folium.FeatureGroup(name="My Map")
+volcanoes = folium.FeatureGroup(name="US Volcanoes")
 
-volcanoes = pandas.read_csv("Volcanoes.csv")
-lats = list(volcanoes["LAT"])
-lons = list(volcanoes["LON"])
-elevs = list(volcanoes["ELEV"])
-names = list(volcanoes["NAME"])
+volcano_data = pandas.read_csv("Volcanoes.csv")
+lats = list(volcano_data["LAT"])
+lons = list(volcano_data["LON"])
+elevs = list(volcano_data["ELEV"])
+names = list(volcano_data["NAME"])
 
 def color_producer(elevation):
     if elevation < 1000:
@@ -19,7 +19,21 @@ def color_producer(elevation):
         return 'red'
 
 for lat, lon, elev, name in zip(lats, lons, elevs, names):
-    features.add_child(folium.Circle(location=[lat, lon], radius=5000, popup=f'{name} - {elev}m', color='gray', weight=2, fill_color=color_producer(elev), fill_opacity=1))
-map.add_child(features)
+    volcanoes.add_child(folium.CircleMarker(location=[lat, lon],
+        radius=7,
+        popup=f'{name} - {elev}m',
+        color='gray',
+        weight=2,
+        fill_color=color_producer(elev),
+        fill_opacity=1))
+
+populations = folium.FeatureGroup(name="World Populations")
+populations.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+    style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 
+    else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(volcanoes)
+map.add_child(populations)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
