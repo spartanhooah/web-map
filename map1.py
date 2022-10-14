b10e6262b9645edd2520f7bd617bc15ea1cc26e1
Ben Frey <spartanhooah@gmail.com>
2022-10-13 20:58:37 -0400

    make markers circles, add elevation to popup

--- map1.py
+++ map1.py
@@ -5,9 +5,21 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 features = folium.FeatureGroup(name="My Map")
 
 volcanoes = pandas.read_csv("Volcanoes.csv")
-for index, row in volcanoes.iterrows():
-    features.add_child(folium.Marker(location=[row["LAT"], row["LON"]], popup=row["NAME"], icon=folium.Icon(color="green")))
+lats = list(volcanoes["LAT"])
+lons = list(volcanoes["LON"])
+elevs = list(volcanoes["ELEV"])
+names = list(volcanoes["NAME"])
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+for lat, lon, elev, name in zip(lats, lons, elevs, names):
+    features.add_child(folium.Circle(location=[lat, lon], radius=5000, popup=f'{name} - {elev}m', color='gray', weight=2, fill_color=color_producer(elev), fill_opacity=1))
 map.add_child(features)
 
 map.save("Map1.html")
